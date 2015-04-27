049eba8f9bbd45568e7b98d90a0bb1dd1c4c2c0b
Thomas Krille <tkrille@gmail.com>
2015-04-27 18:44:15 +0200

    [change] adjust Debian package to usage of Flyway

--- debian/sql/osiam-addon-administration/create_admin.sql
+++ debian/sql/osiam-addon-administration/create_admin.sql
@@ -1,12 +1,12 @@
 --
--- Create group 'admin' and assign user 'marissa'
+-- Create group 'admin' and assign user 'admin'
 --
 INSERT INTO scim_meta (id, created, lastmodified, location, resourcetype, version)
     VALUES (2, '2011-10-10', '2011-10-10', NULL, 'Group', NULL);
 
 INSERT INTO scim_id (internal_id, external_id, id, meta_id)
     VALUES (2, NULL, 'd158a584-f19d-2af3-af36-26ffebe96ae0', 2);
-            
+
 INSERT INTO scim_group VALUES ('admin', 2);
 
 INSERT INTO scim_group_scim_id VALUES (2, 1);
